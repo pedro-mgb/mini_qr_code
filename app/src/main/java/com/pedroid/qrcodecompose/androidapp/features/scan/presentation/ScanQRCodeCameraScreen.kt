b18750fb4a9eb86b0bf8a0cdb5fd793e90ca09ba
Pedro Bilro <pedro.bilro@tecnico.ulisboa.pt>
2024-03-10 18:18:18 +0000

    feat: Align camera scanner frame colour with app theme

--- app/src/main/java/com/pedroid/qrcodecompose/androidapp/features/scan/presentation/ScanQRCodeCameraScreen.kt
+++ app/src/main/java/com/pedroid/qrcodecompose/androidapp/features/scan/presentation/ScanQRCodeCameraScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -50,7 +49,7 @@ fun ScanQRCodeCameraScreen(
         QRCodeComposeXScanner(
             modifier = modifier,
             onResult = onQRCodeResult,
-            frameColor = Color.Green,
+            frameColor = MaterialTheme.colorScheme.secondary,
             frameVerticalPercent = 0.4f,
         )
     }
