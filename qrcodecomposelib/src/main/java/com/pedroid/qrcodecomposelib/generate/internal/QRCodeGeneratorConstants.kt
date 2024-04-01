77772fecbe1e51b17c27734ad4c75c64a860a9f7
pedro-mgb <pedro.mgb@hotmail.com>
2024-04-01 17:50:57 +0100

    fix: Improve image resolution of code bitmap

--- qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/generate/internal/QRCodeGeneratorConstants.kt
+++ qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/generate/internal/QRCodeGeneratorConstants.kt
@@ -6,5 +6,5 @@ import androidx.compose.ui.unit.dp
 internal const val DEFAULT_FILL_COLOR = Color.BLACK
 internal const val DEFAULT_BLANK_COLOR = Color.WHITE
 internal val DEFAULT_PADDING = 16.dp
-internal const val DEFAULT_QR_CODE_SIZE_PX = 500
+internal const val DEFAULT_QR_CODE_SIZE_PX = 1000
 internal const val DEFAULT_QR_CODE_TEXT_ENCODING = "UTF-8"
