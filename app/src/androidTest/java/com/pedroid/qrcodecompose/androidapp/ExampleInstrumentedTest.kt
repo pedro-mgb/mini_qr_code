fe93a038e2dd3d851e0d4683b01a4b31381c2e0e
pedro-mgb <pedro.mgb@hotmail.com>
2024-04-10 13:40:41 +0100

    fix: Fix example instrumented test

--- app/src/androidTest/java/com/pedroid/qrcodecompose/androidapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/pedroid/qrcodecompose/androidapp/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.pedroid.qrcodecompose", appContext.packageName)
+        Assert.assertEquals("com.pedroid.qrcodecompose.androidapp", appContext.packageName)
     }
 }
