b860bcda3e41ad81b48ff9284ccae246e9b47132
Pedro Bilro <pedro.bilro@tecnico.ulisboa.pt>
2024-04-19 19:38:21 +0100

    version: 0.3.0 (3)
    with new app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.pedroid.qrcodecompose.androidapp"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
