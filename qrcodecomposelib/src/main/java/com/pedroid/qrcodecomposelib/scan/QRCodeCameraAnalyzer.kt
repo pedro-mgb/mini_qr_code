0b133ebc0b6f59ebba74b9f24ab831d6df44c261
Pedro Bilro <pedro.bilro@tecnico.ulisboa.pt>
2024-04-28 13:10:16 +0100

    docs: Add code documentation for qrcodecomposelib

--- qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/scan/QRCodeCameraAnalyzer.kt
+++ qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/scan/QRCodeCameraAnalyzer.kt
@@ -2,4 +2,7 @@ package com.pedroid.qrcodecomposelib.scan
 
 import androidx.camera.core.ImageAnalysis
 
+/**
+ * Interface responsible for handling analysis of camera images and detecting QR Codes and barcodes
+ */
 interface QRCodeCameraAnalyzer : QRCodeAnalyzerWithResult, ImageAnalysis.Analyzer
--- qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/scan/QRCodeFileAnalyzer.kt
+++ qrcodecomposelib/src/main/java/com/pedroid/qrcodecomposelib/scan/QRCodeFileAnalyzer.kt
@@ -3,6 +3,9 @@ package com.pedroid.qrcodecomposelib.scan
 import android.content.Context
 import android.net.Uri
 
+/**
+ * Interface responsible for handling analysis of file images and detecting QR Codes and barcodes
+ */
 interface QRCodeFileAnalyzer : QRCodeAnalyzerWithResult {
     fun analyze(
         context: Context,
