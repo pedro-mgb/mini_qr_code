c21ccad22f8fd04f7df9fa1419f3403998b64c10
Pedro Bilro <47814970+pedro-mgb@users.noreply.github.com>
2024-05-18 20:12:59 +0100

    App version 0.5.0 (#22)
    * version: 0.5.0 (5)
    
    * fix: protobuf proguard for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.pedroid.qrcodecompose.androidapp"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
