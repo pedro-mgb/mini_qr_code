d31669545e8f14c9779c3e82567317c176ac1a93
pedro-mgb <pedro.mgb@hotmail.com>
2024-05-31 18:05:33 +0100

    Add navigation compose dependency
    2.8.0 supports safe args (still in beta)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,6 +108,7 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room)
     implementation(libs.androidx.room.ktx)
     implementation(libs.arrow.optics)
