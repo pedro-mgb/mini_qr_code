e277c1af64794bf8200676615fac4c9bf242576b
pedro-mgb <pedro.mgb@hotmail.com>
2024-07-10 16:11:39 +0100

    fix: revert build.gradle.kts changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pedroid.qrcodecompose.androidapp"
         minSdk = 21
         targetSdk = 34
-	versionCode = 8
-        versionName = "0.5.1"
+	    versionCode = 7
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
