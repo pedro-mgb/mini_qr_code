8f0f204fde2a75b3375229a5ac17640e2657eb8e
Pedro Bilro <pedro-mgb@users.noreply.github.com>
2025-06-10 16:06:24 +0000

    App version 0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.pedroid.qrcodecompose.androidapp"
         minSdk = 21
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.7.0"
+        versionCode = 10
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
